c1f0725017f356022fca62761bb7e6dd2c9b3300
loryzike1 <azike4@gmail.com>
2022-07-06 17:37:22 -0700

    Make

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - prints putchar.
+ *
+ *Return: Always 0.
+ **/
+
+	int main(void)
+{
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+		putchar('\n');
+	Return(0);
+
+}
